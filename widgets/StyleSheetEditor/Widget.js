b6391026e54f514b537fbbb04feb2ecab4421b96
Geotics-Lab <89240420+Geotics-Lab@users.noreply.github.com>
2021-08-30 16:39:34 +0200

    Add files via upload

---
+++ widgets/StyleSheetEditor/Widget.js
@@ -0,0 +1,45 @@
+///////////////////////////////////////////////////////////////////////////
+// Copyright © Esri. All Rights Reserved.
+//
+// Licensed under the Apache License Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+///////////////////////////////////////////////////////////////////////////
+define(["dojo/_base/declare",
+	"jimu/BaseWidget",
+	"dijit/_WidgetsInTemplateMixin",
+],
+	function (declare, BaseWidget, _WidgetsInTemplateMixin) {
+		return declare([BaseWidget, _WidgetsInTemplateMixin], {
+
+			name: "AddData",
+			baseClass: "jimu-widget-add-data",
+
+			postCreate: function () {
+				this.inherited(arguments);
+			},
+
+			startup: function () {
+
+				var styleSheet = document.createElement("style")
+				styleSheet.type = "text/css"
+				styleSheet.innerText = this.config.style
+				document.head.appendChild(styleSheet)
+
+
+			}
+
+
+
+
+		});
+
+	});
