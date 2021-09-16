15effb0004fa1c5485489b9c794b47b5766c2f37
Geotics-Lab <89240420+Geotics-Lab@users.noreply.github.com>
2021-09-16 11:56:22 +0200

    Add files via upload

---
+++ widgets/AdvancedSettings/setting/nls/strings.js
@@ -0,0 +1,62 @@
+define({
+  root: ({
+    numPerPage: "Number of items per page",
+    scopeOptions: {
+      "defaultScope": "Default search scope",
+      "labelPlaceholder": "Optional label",
+      "MyContent": "Allow My Content",
+      "MyOrganization": "Allow My Organization",
+      "ArcGISOnline": "Allow ArcGIS Online",
+      "Curated": "Allow Curated",
+      "CuratedFilter": "Curated filter",
+      "livingAtlasExample": "Living atlas example:"
+    },
+    addFromUrl: {
+      caption: "Allow URL"
+    },
+    addFromFile: {
+      caption: "Allow File",
+      maxRecordCount: "Maximum records per file"
+    },
+    _default: "Default",
+    makeDefault: "Make default"
+  }),
+  "ar": 0,
+  "bs": 0,
+  "ca": 0,
+  "cs": 0,
+  "da": 0,
+  "de": 0,
+  "el": 0,
+  "es": 0,
+  "et": 0,
+  "fi": 0,
+  "fr": 0,
+  "he": 0,
+  "hr": 0,
+  "hu": 0,
+  "it": 0,
+  "id": 0,
+  "ja": 0,
+  "ko": 0,
+  "lt": 0,
+  "lv": 0,
+  "nb": 0,
+  "nl": 0,
+  "pl": 0,
+  "pt-br": 0,
+  "pt-pt": 0,
+  "ro": 0,
+  "ru": 0,
+  "sk": 0,
+  "sl": 0,
+  "sr": 0,
+  "sv": 0,
+  "th": 0,
+  "tr": 0,
+  "uk": 0,
+  "vi": 0,
+  "zh-cn": 0,
+  "zh-hk": 0,
+  "zh-tw": 0
+});
