175b0d63b0f4f66a6b05c5a9c4ee553561026651
Geotics-Lab <89240420+Geotics-Lab@users.noreply.github.com>
2021-10-22 11:20:38 +0200

    Add files via upload

--- widgets/AdvancedSettings/setting/Setting.js
+++ widgets/AdvancedSettings/setting/Setting.js
@@ -1,18 +1,5 @@
-///////////////////////////////////////////////////////////////////////////
-// Copyright © Esri. All Rights Reserved.
-//
-// Licensed under the Apache License Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-///////////////////////////////////////////////////////////////////////////
+
+
 define(["dojo/_base/declare",
 
   'jimu/LayerInfos/LayerInfos',
@@ -38,57 +25,30 @@ define(["dojo/_base/declare",
       },
 
       startup: function () {
+
+
         var self = this
-        if (this._started) {
-          return;
-        }
-        this.layerInfos = LayerInfos.getInstanceSync()._layerInfos;
         this.inherited(arguments);
-        this.setConfig(this.config);
 
-        console.log(this)
+        require(['https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.62.3/codemirror.min.js'], function (CodeMirror) {
 
-        this.setVisibilityTogglingFilter()
+          self.CodeMirror = CodeMirror
 
 
-        this['import-config'].onchange = function (e) {
-          var files = document.getElementById('import-config').files;
-          console.log(files);
-          if (files.length <= 0) {
-            return false;
+          if (this._started) {
+            return;
           }
-        
-          var fr = new FileReader();
-        
-          fr.onload = function(e) { 
-          console.log(e);
-            var result = JSON.parse(e.target.result);
-            console.log(result)
-            self.config = result
-          }
-        
-          fr.readAsText(files.item(0));
-          
-        }
-        this['export-config'].onclick = function (e) {
-          var saveData = (function () {
-            var a = document.createElement("a");
-            // document.body.appendChild(a);
-            // a.style = "display: none";
-            return function (data, fileName) {
-                var json = JSON.stringify(data),
-                    blob = new Blob([json], {type: "octet/stream"}),
-                    url = window.URL.createObjectURL(blob);
-                a.href = url;
-                a.download = fileName;
-                a.click();
-                window.URL.revokeObjectURL(url);
-            };
-            }());
-            
-    
-            saveData(self.config, "config.json");
-        }
+          self.layerInfos = LayerInfos.getInstanceSync()._layerInfos;
+
+          self.setConfig(self.config);
+
+          console.log(self)
+
+          self.setVisibilityTogglingFilter()
+          self.setCustomScript()
+          self.setCustomCss()
+
+        })
       },
 
       setVisibilityTogglingFilter: function (params) {
@@ -241,6 +201,222 @@ define(["dojo/_base/declare",
 
       },
 
+      setCustomScript: function () {
+        var self = this
+
+        var index = 0
+        this.config.customScript.forEach(element => {
+          var li = document.createElement("li")
+          li.innerHTML = element.name
+          li.classList.add("list-group-item")
+          li.setAttribute("script-name", element.name)
+          li.setAttribute("script-index", index)
+          li.setAttribute("script-content", element.content)
+
+          li.onclick = function (e) {
+            var name = this.getAttribute('script-name')
+            var content = this.getAttribute('script-content')
+            var indexSelected = this.getAttribute('script-index')
+            self["custom-script-content"].setAttribute('selected', indexSelected)
+            self["custom-script-content"].value = content
+            self["custom-script-name"].value = name
+            self['save-script'].style.display = "inline"
+            self['remove-script'].style.display = "inline"
+            self['add-script'].style.display = "none"
+            self["custom-script-type"].setAttribute('disabled')
+            self["custom-script-target"].setAttribute('disabled')
+
+          }
+          this["script-list"].appendChild(li)
+          index++
+        });
+
+        this["add-script"].onclick = function (e) {
+          self.config.customScript.push(JSON.parse(JSON.stringify({
+            name: self["custom-script-name"].value,
+            content: self["custom-script-content"].value,
+            format: self["custom-script-type"].value,
+            target: self["custom-script-target"].value,
+          })))
+
+          var li = document.createElement("li")
+          li.innerHTML = self["custom-script-name"].value
+          li.classList.add("list-group-item")
+          li.setAttribute("script-name", self["custom-script-name"].value)
+          li.setAttribute("script-index", index)
+          li.setAttribute("script-content", self["custom-script-content"].value)
+          li.onclick = function (e) {
+            var name = this.getAttribute('script-name')
+            var content = this.getAttribute('script-content')
+            var indexSelected = this.getAttribute('script-index')
+            self["custom-script-content"].setAttribute('selected', indexSelected)
+            self["custom-script-content"].value = content
+            self["custom-script-name"].value = name
+            self['save-script'].style.display = "inline"
+            self['remove-script'].style.display = "inline"
+            self['add-script'].style.display = "none"
+            self["custom-script-type"].setAttribute('disabled')
+            self["custom-script-target"].setAttribute('disabled')
+
+          }
+          self["script-list"].appendChild(li)
+
+          self["custom-script-name"].value = ""
+          self["custom-script-content"].value = ""
+
+          index++
+          console.log("config", self.config)
+        }
+
+        this["save-script"].onclick = function (e) {
+          var indexSelected = self["custom-script-content"].getAttribute('selected')
+
+          self.config.customScript[indexSelected] = {
+            name: self["custom-script-name"].value,
+            content: self["custom-script-content"].value
+          }
+          self["script-list"].querySelectorAll('[script-index="' + indexSelected + '"]')[0].setAttribute("script-name", self["custom-script-name"].value)
+          self["script-list"].querySelectorAll('[script-index="' + indexSelected + '"]')[0].setAttribute("script-content", self["custom-script-content"].value)
+          self["script-list"].querySelectorAll('[script-index="' + indexSelected + '"]')[0].innerHTML = self["custom-script-name"].value
+
+
+          self['add-script'].style.display = "inline"
+          self['save-script'].style.display = "none"
+          self['remove-script'].style.display = "none"
+          self["custom-script-name"].value = ""
+          self["custom-script-content"].value = ""
+
+
+          self["custom-script-type"].removeAttribute('disabled')
+          self["custom-script-target"].removeAttribute('disabled')
+
+        }
+
+        this["remove-script"].onclick = function (e) {
+          var indexSelected = self["custom-script-content"].getAttribute('selected')
+          self['save-script'].style.display = "none"
+          self['remove-script'].style.display = "none"
+          self['add-script'].style.display = "inline"
+          self["custom-script-name"].value = ""
+          self["custom-script-content"].value = ""
+          self["script-list"].querySelectorAll('[script-index="' + indexSelected + '"]')[0].remove()
+
+          for (let count = 0; count < self["script-list"].querySelectorAll('[script-index]').length; count++) {
+            self["script-list"].querySelectorAll('[script-index]')[count].setAttribute('script-index', count)
+
+          }
+          self["custom-script-type"].removeAttribute('disabled')
+          self["custom-script-target"].removeAttribute('disabled')
+          self.config.customScript.splice(indexSelected, 1)
+
+          index--
+        }
+
+      },
+
+      setCustomCss: function () {
+        var self = this
+
+        var index = 0
+        this.config.customCss.forEach(element => {
+          var li = document.createElement("li")
+          li.innerHTML = element.name
+          li.classList.add("list-group-item")
+          li.setAttribute("css-name", element.name)
+          li.setAttribute("css-index", index)
+          li.setAttribute("css-content", element.content)
+
+          li.onclick = function (e) {
+            var name = this.getAttribute('css-name')
+            var content = this.getAttribute('css-content')
+            var indexSelected = this.getAttribute('css-index')
+            self["custom-css-content"].setAttribute('selected', indexSelected)
+            self["custom-css-content"].value = content
+            self["custom-css-name"].value = name
+            self['save-css'].style.display = "inline"
+            self['remove-css'].style.display = "inline"
+            self['add-css'].style.display = "none"
+
+          }
+          this["css-list"].appendChild(li)
+          index++
+        });
+
+        this["add-css"].onclick = function (e) {
+          self.config.customCss.push(JSON.parse(JSON.stringify({
+            name: self["custom-css-name"].value,
+            content: self["custom-css-content"].value
+          })))
+
+          var li = document.createElement("li")
+          li.innerHTML = self["custom-css-name"].value
+          li.classList.add("list-group-item")
+          li.setAttribute("css-name", self["custom-css-name"].value)
+          li.setAttribute("css-index", index)
+          li.setAttribute("css-content", self["custom-css-content"].value)
+          li.onclick = function (e) {
+            var name = this.getAttribute('css-name')
+            var content = this.getAttribute('css-content')
+            var indexSelected = this.getAttribute('css-index')
+            self["custom-css-content"].setAttribute('selected', indexSelected)
+            self["custom-css-content"].value = content
+            self["custom-css-name"].value = name
+            self['save-css'].style.display = "inline"
+            self['remove-css'].style.display = "inline"
+            self['add-css'].style.display = "none"
+
+          }
+          self["css-list"].appendChild(li)
+
+          self["custom-css-name"].value = ""
+          self["custom-css-content"].value = ""
+          
+          index++
+          console.log("config", self.config)
+        }
+
+        this["save-css"].onclick = function (e) {
+          var indexSelected = self["custom-css-content"].getAttribute('selected')
+
+          self.config.customCss[indexSelected] = {
+            name: self["custom-css-name"].value,
+            content: self["custom-css-content"].value
+          }
+          self["css-list"].querySelectorAll('[css-index="' + indexSelected + '"]')[0].setAttribute("css-name", self["custom-css-name"].value)
+          self["css-list"].querySelectorAll('[css-index="' + indexSelected + '"]')[0].setAttribute("css-content", self["custom-css-content"].value)
+          self["css-list"].querySelectorAll('[css-index="' + indexSelected + '"]')[0].innerHTML = self["custom-css-name"].value
+
+
+          self['add-css'].style.display = "inline"
+          self['save-css'].style.display = "none"
+          self['remove-css'].style.display = "none"
+          self["custom-css-name"].value = ""
+          self["custom-css-content"].value = ""
+
+
+        }
+
+        this["remove-css"].onclick = function (e) {
+          var indexSelected = self["custom-css-content"].getAttribute('selected')
+          self['save-css'].style.display = "none"
+          self['remove-css'].style.display = "none"
+          self['add-css'].style.display = "inline"
+          self["custom-css-name"].value = ""
+          self["custom-css-content"].value = ""
+          self["css-list"].querySelectorAll('[css-index="' + indexSelected + '"]')[0].remove()
+
+          for (let count = 0; count < self["css-list"].querySelectorAll('[css-index]').length; count++) {
+            self["css-list"].querySelectorAll('[css-index]')[count].setAttribute('css-index', count)
+
+          }
+
+          self.config.customCss.splice(indexSelected, 1)
+
+          index--
+        }
+
+      },
+
       getConfig: function () {
 
 
@@ -261,3 +437,5 @@ define(["dojo/_base/declare",
     });
 
   });
+
+   
